eb5917af85a5ccf8db6f2f8de75570a485c0cf7d
bank2711 <bbenchy2021@gmail.com>
2024-12-22 14:19:11 +0700

    b

--- script.js
+++ script.js
@@ -36,6 +36,7 @@ document.body.addEventListener("keydown", (e) => {
   }
 });
 
+
 // Animating work instances on scroll
 
 workImgs.forEach((workImg) => workImg.classList.add("transform"));
@@ -91,24 +92,6 @@ document.body.addEventListener("keydown", (e) => {
   }
 });
 
-// Rotating logos animation
 
-const logosWrappers = document.querySelectorAll(".logo-group");
-
-const sleep = (number) => new Promise((res) => setTimeout(res, number));
-
-logosWrappers.forEach(async (logoWrapper, i) => {
-  const logos = Array.from(logoWrapper.children);
-  await sleep(1400 * i);
-  setInterval(() => {
-    let temp = logos[0];
-    logos[0] = logos[1];
-    logos[1] = logos[2];
-    logos[2] = temp;
-    logos[0].classList.add("hide", "to-top");
-    logos[1].classList.remove("hide", "to-top", "to-bottom");
-    logos[2].classList.add("hide", "to-bottom");
-  }, 5600);
-});
 
 yearEl.textContent = new Date().getFullYear();
